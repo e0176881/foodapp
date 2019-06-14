43bcb31d4543d46d975a09a6432c4f1aa82a6dd9
mingxuan <e0176881@u.nus.edu>
2019-06-14 16:17:19 +0800

    missed out the package for firebase database

--- android/app/src/main/java/com/foodapp/MainApplication.java
+++ android/app/src/main/java/com/foodapp/MainApplication.java
@@ -28,7 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
           new MainReactPackage(),
             new RNGestureHandlerPackage(),
             new RNFirebasePackage(),
-            new RNFirebaseAuthPackage()
+            new RNFirebaseAuthPackage(),
+            new RNFirebaseDatabasePackage()
       );
     }
 
