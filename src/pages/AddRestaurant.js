85a6e14d98cfb80d4c58b73928669ba015892491
mingxuan <e0176881@u.nus.edu>
2019-06-22 01:11:27 +0800

    give a title to the back button at addRestaurantPage, added addReview Function, readReview , updateReview

--- src/pages/AddRestaurant.js
+++ src/pages/AddRestaurant.js
@@ -1,74 +1,126 @@
-import React, { Component } from 'react';
-import { NavigationActions,HeaderBackButton} from 'react-navigation';
+import React, {
+    Component
+} from 'react';
+import {
+    NavigationActions,
+    HeaderBackButton
+} from 'react-navigation';
 import firebase from 'react-native-firebase';
-import { StyleSheet, Text, TextInput, View, TouchableOpacity } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    TouchableOpacity
+} from 'react-native';
+
 
 
 export default class AddRestaurant extends React.Component {
-    static navigationOptions = ({ navigation }) => {
+    static navigationOptions = ({
+        navigation
+    }) => {
         return {
-          headerLeft: (
-            <HeaderBackButton
-              onPress={() =>navigation.navigate(NavigationActions.navigate({
-                routeName: 'Bottom',
-                action: NavigationActions.navigate({ routeName: 'Home' })
-              }))}
+            headerLeft: (<
+                HeaderBackButton onPress={
+                    () => navigation.navigate(NavigationActions.navigate({
+                        routeName: 'Bottom',
+                        action: NavigationActions.navigate({
+                            routeName: 'Home'
+                        })
+                    }))
+                }
             />
-          )
+            ),
+            title: 'Add Review',
         };
-      }
+    }
     constructor(props) {
         super(props);
         this.state = {
             restaurantName: '',
-            address: '',
-            openingHours: '',
-            closingHours: '',
-            phoneNumber: '',
-            website: '',
+            foodPrice: '',
+            description: '',
+            rating: '',
+            testDescription: '',
+            uniqueKey: '',
             errorMessage: null
         };
-        this.addRestaurant = this.addRestaurant.bind(this);
+        this.addReview = this.addReview.bind(this);
+        this.readReview = this.readReview.bind(this);
+        this.updateReview = this.updateReview.bind(this);
     }
 
     componentDidMount() {
         firebase.auth().onAuthStateChanged((user) => {
             if (user) {
                 this.setState({
-                    loginName: user.email
+                    loginName: user.email,
+                    uid: user.uid
                 })
             }
         });
     }
 
-    addRestaurant = () => {
-        const { restaurantName, address, openingHours, closingHours, phoneNumber, website } = this.state
+    addReview = () => {
+        const {
+            restaurantName,
+            foodPrice,
+            description,
+            rating
+        } = this.state
         firebase
             .database()
-            .ref()
-            .child("restaurants")
-            .set({
-                restaurantName: {
-                    address: address,
-                    openingHours: openingHours,
-                    closingHours: closingHours,
-                    phoneNumber: phoneNumber,
-                    website: website
-                }
+            .ref('reviews')
+            .child('reviews/' + this.state.uid && this.state.uid)
+            .push({
+                restaurantName: restaurantName,
+                foodPrice: foodPrice,
+                description: description,
+                rating: rating
             })
             .then((response) => {
-                alert("Restaurant added successfully!")
+                alert("Review added successfully!")
             })
-            .catch(error => this.setState({ errorMessage: error.message }))
+            .catch(error => alert(error.message))
+    }
+
+    readReview = () => {
+        var userId = firebase.auth().currentUser.uid;
+        firebase.database().ref('/reviews/' + userId).once('value').then(function (snapshot) {
+            snapshot.forEach(function (childSnapshot) {
+                var description = (childSnapshot.val() && childSnapshot.val().restaurantName)
+                var uniqueKey = (childSnapshot.key && childSnapshot.key)
+                this.setState({
+                    testDescription: description,
+                    uniqueKey: uniqueKey
+                })
+            }.bind(this))
+        }.bind(this));
+
+    }
+
+    updateReview = () => {
+
+        firebase
+            .database()
+            .ref('reviews')
+            .child('reviews/' + this.state.uid && this.state.uid)
+            .child(this.state.uniqueKey && this.state.uniqueKey)
+            .set({
+                restaurantName: this.state.testDescription && this.state.testDescription,
+                foodPrice: 'idk',
+                description: 'idk',
+                rating: 'idk'
+            })
+            .then((response) => {
+                alert("Review updated successfully!")
+            })
+            .catch(error => alert(error.message))
     }
 
 
     render() {
-
-        //firebase.auth().onAuthStateChanged(function (user) {
-        //  if (user) {
-        // User is signed in.
-
         return (
             <View style={styles.container}>
                 {this.state.errorMessage &&
@@ -83,66 +135,62 @@ export default class AddRestaurant extends React.Component {
                     selectionColor="#fff"
                     onChangeText={restaurantName => this.setState({ restaurantName })}
                     value={this.state.restaurantName}
-                    onSubmitEditing={() => this.address.focus()}
+                    onSubmitEditing={() => this.foodPrice.focus()}
                 />
 
                 <TextInput style={styles.inputBox}
                     underlineColorAndroid='rgba(0,0,0,0)'
-                    placeholder="Address"
+                    placeholder="Food Price"
                     placeholderTextColor="#ffffff"
                     selectionColor="#fff"
-                    onChangeText={address => this.setState({ address })}
-                    value={this.state.address}
-                    onSubmitEditing={() => this.openingHours.focus()}
+                    onChangeText={foodPrice => this.setState({ foodPrice })}
+                    value={this.state.foodPrice}
+                    onSubmitEditing={() => this.description.focus()}
                 />
 
 
                 <TextInput style={styles.inputBox}
                     underlineColorAndroid='rgba(0,0,0,0)'
-                    placeholder="Opening Hours"
-                    placeholderTextColor="#ffffff"
-                    selectionColor="#fff"
-                    keyboardType="numeric"
-                    onChangeText={openingHours => this.setState({ openingHours })}
-                    value={this.state.openingHours}
-                    onSubmitEditing={() => this.closingHours.focus()}
-                />
-
-                <TextInput style={styles.inputBox}
-                    underlineColorAndroid='rgba(0,0,0,0)'
-                    placeholder="Closing Hours"
+                    placeholder="Description"
                     placeholderTextColor="#ffffff"
                     selectionColor="#fff"
                     keyboardType="numeric"
-                    onChangeText={closingHours => this.setState({ closingHours })}
-                    value={this.state.closingHours}
+                    onChangeText={description => this.setState({ description })}
+                    value={this.state.description}
+                    onSubmitEditing={() => this.rating.focus()}
+                />
+
+                <TextInput style={styles.inputBox}
+                    underlineColorAndroid='rgba(0,0,0,0)'
+                    placeholder="Rating"
+                    placeholderTextColor="#ffffff"
+                    selectionColor="#fff"
+                    keyboardType="numeric"
+                    onChangeText={rating => this.setState({ rating })}
+                    value={this.state.rating}
                     onSubmitEditing={() => this.phoneNumber.focus()}
+                    ref={(input) => this.rating = input}
                 />
+                <TouchableOpacity style={styles.button}>
+                    <Text onPress={this.addReview} style={styles.buttonText}>Add Review</Text>
+                </TouchableOpacity>
 
                 <TextInput style={styles.inputBox}
                     underlineColorAndroid='rgba(0,0,0,0)'
-                    placeholder="Phone Number"
+                    placeholder="Restoran Name"
                     placeholderTextColor="#ffffff"
                     selectionColor="#fff"
-                    keyboardType="phone-pad"
-                    onChangeText={phoneNumber => this.setState({ phoneNumber })}
-                    value={this.state.phoneNumber}
-                    onSubmitEditing={() => this.website.focus()}
-                />
-
-
-                <TextInput style={styles.inputBox}
-                    underlineColorAndroid='rgba(0,0,0,0)'
-                    placeholder="Website"
-                    placeholderTextColor="#ffffff"
-                    selectionColor="#fff"
-                    onChangeText={website => this.setState({ website })}
-                    value={this.state.website}
-                    onSubmitEditing={() => this.website.focus()}
+                    keyboardType="numeric"
+                    onChangeText={testDescription => this.setState({ testDescription })}
+                    value={this.state.testDescription && this.state.testDescription}
                 />
 
                 <TouchableOpacity style={styles.button}>
-                    <Text onPress={this.addRestaurant} style={styles.buttonText}>Add Restaurant</Text>
+                    <Text onPress={this.readReview} style={styles.buttonText}>Read Review</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.button}>
+                    <Text onPress={this.updateReview} style={styles.buttonText}>Update Review</Text>
                 </TouchableOpacity>
 
             </View>
